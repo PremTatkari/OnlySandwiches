b008a6ab1cd0e5c84f102a6f0938346288321a97
Prem Tatkari <tatkariprem2002@gmail.com>
2024-06-06 17:15:09 +0530

    Reverted changes

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -3,7 +3,7 @@ import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-const apiKey = process.env.API_KEY_I18N;
+const apiKey = "jTt-xHUlGv55sj4vJJWphQ";
 const loadPath = `https://api.i18nexus.com/project_resources/translations/{{lng}}/{{ns}}.json?api_key=${apiKey}`;
 
 i18next
